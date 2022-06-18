caee82834170517942e99b5120042772341b798f
prestij1987 <lehaleyba@mail.ru>
2022-06-18 15:57:10 +0300

    initial Commit2

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import BasePermission
 class IsOwner(BasePermission):
     # def has_permission(self, request, view):
     def has_object_permission(self, request, view, objects):
-        return request.user == user.objects
+        return request.user == object.creator or request.user.is_superuser
 
 
 
